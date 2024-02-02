f4c519239728e45034484e36a9f4e64192848100
Melnik <melnik721ivan@gmail.com>
2024-02-02 21:09:26 +0300

    fix calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -3,7 +3,7 @@ from random import randint, choice
 
 def brain_calc():
     title = 'What is the result of the expression?'
-    num1 = randint(5, 10)
+    num1 = randint(1, 5)
     num2 = randint(1, 5)
     operator = choice('+-*')
     question = f'{num1} {operator} {num2}'
